97218766c5ea03a1184156e4a33be99d776c5b8c
Joe Burgess <joe@flatironschool.com>
2014-01-07 05:12:46 -0500

    Adding docs to private JMMViewController methods

--- CalcTest/JMMCalculatorViewController.m
+++ CalcTest/JMMCalculatorViewController.m
@@ -26,8 +26,11 @@
     return self;
 }
 
-/// @name Documented Private Methods
+/// @name Private Methods
 
+/**
+ *  On view load, run setActionForButtons: on all buttons in `buttonView` and intialize a new [JMMCalculator](JMMCalculator)
+ */
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -61,6 +64,14 @@
 
 #pragma mark - Private Methods
 
+/**
+ *  When `UIButton` tapped is the equals button call [JMMCalculator equate] 
+ *  and set the text of `displayLabel` to the result of the calculation.
+ *
+ *  Before equating, if no operation was just pressed add the current display text
+ *  as an operand to the calculator.
+ */
+
 -(void) handleEquals {
     if (!operationJustPressed) {
         NSString *currentDisplayedText = self.displayLabel.text;
@@ -71,6 +82,14 @@
     operationJustPressed = YES;
 }
 
+/**
+ *  When `UIButton` tapped is an operand button, pass the operand to the [JMMCalculator captureOperand]
+ *  and [JMMCalculator captureOperation] method. This operand will be used later for in the [JMMCalculator equate]
+ *  operation.
+ *
+ *  @param title Title of the operation that was tapped. Passed in from handleButtonWithTitlePressed:
+ */
+
 -(void) handleOperationPressed:(NSString *)title {
     operationJustPressed = YES;
     NSString *currentDisplayedText = self.displayLabel.text;
@@ -79,6 +98,11 @@
     
 }
 
+/**
+ *  When `UIButton` tapped is a numeral, concatenate numeral to the `displayLabel`
+ *
+ *  @param title The numeral that was pressed.
+ */
 -(void) handleNumeralPressed:(NSString *)title {
     NSString *currentDisplayedText = self.displayLabel.text;
     
@@ -91,9 +115,26 @@
     operationJustPressed = NO;
 }
 
+/**
+ *  Checks whether title is an operation by comparing it to the `JMMCalcOperation`
+ *
+ *  @param title The title of the `UIButton` pressed
+ *
+ *  @return `YES` or `NO`
+ */
 -(BOOL) buttonIsAnOperation:(NSString *)title {
     return [self operationForString:title] != NoNOp;
 }
+
+/**
+ *  Translates the `NSString` representation of an operation to it's appropriate `JMMCalcOperation`
+ *  
+ *  @warning Division not implemented.
+ *
+ *  @param stringOp `NSString` calcualtor operation
+ *
+ *  @return The translated `JMMCalcOperation`
+ */
 -(JMMCalcOperation) operationForString:(NSString *)stringOp {
     if ([stringOp isEqualToString:@"+"])
         return AdditionOp;
